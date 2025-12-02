99b2bbcc7cc5e0398cf229e13459c27481c58270
Shubham Rawat <shubhamrawat1808@gmail.com>
2025-12-02 21:35:42 +0530

    Initial Commit

---
+++ src/main/java/com/college/career/AI_BasedCareerRecommendationSystem/AIBasedCareerRecommendationSystemApplication.java
@@ -0,0 +1,13 @@
+package com.college.career.AI_BasedCareerRecommendationSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AIBasedCareerRecommendationSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AIBasedCareerRecommendationSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/college/career/AI_BasedCareerRecommendationSystem/AiBasedCareerRecommendationSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.college.career.AI_BasedCareerRecommendationSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiBasedCareerRecommendationSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
